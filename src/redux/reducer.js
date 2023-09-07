e1b04f99b866cbd07f5c99cd4d398fddcecb2acc
LocoGOD <darioperez201864@gmail.com>
2023-09-07 18:37:47 -0300

    Configuracion Inicial

---
+++ src/redux/reducer.js
@@ -0,0 +1,13 @@
+// Estado global
+const initialState = {
+} 
+
+// Reducer 
+const reducer = (state = initialState, action) => {
+   
+    switch (action.type){
+        default: return {...state}
+    }
+}
+
+export default reducer;
