c81dd8fe867750278f9b3ae0b7863831226d2fb2
ptsuarez91@hotmail.com <ptsuarez91@hotmail.com>
2023-09-16 17:32:03 -0300

    rutas y boton

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,77 @@
-import "./Navbar.css"
-import { Link } from "react-router-dom";
+import './Navbar.css';
+import { Link } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
+import logo from "../../assets/Logos/logoB.png"
 
-const Navbar = () =>{
-    return(
+
+const Navbar = () => {
+    
+  const { loginWithRedirect, isAuthenticated, user, logout } = useAuth0();
+    
+  return (
         <section className="container-fluid navbar">
-              <div>           
-                <Link to="/"><img src="./Imagenes/Logos/logoB.png" id="logo" alt="logo" /></Link>
+
+              <div id='logoContainer'>           
+                <Link to="/"><img src={logo} id="logo" alt="logo" /></Link>
               </div>
-              
+
+
               <div className="options">
-                <Link to="/servicios" style={{ textDecoration:'none'}}><h5>Servicios</h5></Link>
-                <Link to="/contacto" style={{ textDecoration:'none'}}><h5>Contacto</h5></Link>
-                <Link to="/ofertas" style={{ textDecoration:'none'}}><h5>Ofertas</h5></Link>
-                <Link to="/login"><button>Ingresá</button></Link>
-              </div>    
-        </section>)}
+               
+                <div class="dropdown">
+                 <h5 class="dropdown-toggle" data-bs-toggle="dropdown">Servicios</h5>
+                    <ul class="dropdown-menu">
+                      <li><Link to="/servicios" style={{ textDecoration:'none'}}><span class="dropdown-item">Ver todos</span></Link></li>
+                      <li><Link to="/servicios/luz" style={{ textDecoration:'none'}}><span class="dropdown-item">Luz</span></Link></li>
+                      <li><Link to="/servicios/gas" style={{ textDecoration:'none'}}><span class="dropdown-item">Gas</span></Link></li>
+                      <li><Link to="/servicios/internet" style={{ textDecoration:'none'}}><span class="dropdown-item">Internet</span></Link></li>
+                      <li><Link to="/servicios/agua" style={{ textDecoration:'none'}}><span class="dropdown-item">Agua</span></Link></li>
+                      <li><Link to="/servicios/cable" style={{ textDecoration:'none'}}><span class="dropdown-item">Cable</span></Link></li>
+                      <li><Link to="/servicios/telefonia" style={{ textDecoration:'none'}}><span class="dropdown-item">Telefonia</span></Link></li>
+                      <li><Link to="/servicios/streaming" style={{ textDecoration:'none'}}><span class="dropdown-item">Streaming</span></Link></li>
+                    </ul>
+                </div>
+
+                <h5><Link className='linkh5' to="/contacto">Contacto</Link></h5>
+                <h5><Link className='linkh5' to="/clasificados">Clasificados</Link></h5>
+        
+                
+
+                {isAuthenticated ? (
+                    <div class="dropdown">
+                        <button
+                            class="btn btn-secondary dropdown-toggle"
+                            type="button"
+                            id="dropdownMenuButton1"
+                            data-bs-toggle="dropdown"
+                            aria-expanded="false"
+                            className="ProfileBtn"
+                        >
+                            <img src={user.picture} alt="userImage" className="img" />
+                        </button>
+                        <ul
+                            class="dropdown-menu"
+                            aria-labelledby="dropdownMenuButton1"
+                        >
+                            <li>
+                                <Link to="/usuario" style={{ textDecoration:'none'}}>
+                                    <a class="dropdown-item userDropdown" href=".">Ver mi perfil</a></Link>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href=".">
+                                    <p className="userDropdown" onClick={logout}>Salir</p>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                ) : (
+                    <button className="button" onClick={() => loginWithRedirect()}>
+                        <span>Ingresa</span>
+                        </button>
+                )}
+            </div>
+        </section>
+    );
+};
 
 export default Navbar;
