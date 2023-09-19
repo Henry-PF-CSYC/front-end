3f5a33ff136be8ac5e88e0553e134767e4e353e3
LocoGOD <darioperez201864@gmail.com>
2023-09-19 20:45:05 -0300

    Filtros back

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,4 +1,4 @@
-import { GETPAGINATEDSERVICES, GETSERVICES, GETUSER, SET_CURRENT_PAGE, SET_TOTAL_PAGES, EMPTY_USER, ORDER_BY_NAME, FILTER_BY_TYPE} from "./action-types";
+import { GETSERVICES, GETUSER, GETPAGINATEDSERVICES, EMPTY_USER } from "./action-types";
 
 
 // Estado global
@@ -6,12 +6,10 @@ const initialState = {
     dataUser: {},
     services:[],
     backUpServices:[],
-    
-    currentPage: 1,
     totalPages: 1, 
     currentServicesPage: [],
 } 
-
+console.log(initialState.currentServicesPage)
 
 // Reducer
 const reducer = (state = initialState, action) => {
@@ -21,52 +19,23 @@ const reducer = (state = initialState, action) => {
                 ...state,
                 dataUser: action.payload
             };
-
         case EMPTY_USER:
             return {
                 ...state,
                 dataUser: {}
             };
-            
         case GETSERVICES:
             return{
                 ...state,
                 services: action.payload,
                 backUpServices: action.payload
-            };
-
-        // case GETPAGINATEDSERVICES:
-        //     return {
-        //     ...state,
-        //     currentServicesPage: action.payload,
-        //     };
-
-        // case SET_CURRENT_PAGE:
-        //     return {
-        //         ...state,
-        //         currentPage: action.payload,
-        //     };
-
-        // case SET_TOTAL_PAGES:
-        //     return {
-        //         ...state,
-        //         totalPages: action.payload,
-        //     };
-
-        case ORDER_BY_NAME:
-            return{
-                ...state, 
-                services: action.payload,
-                backUpServices :action.payload,
             }
-
-        case FILTER_BY_TYPE:
-            return{
-                ...state, 
-                services: action.payload,
-                backUpServices :action.payload,
+        case GETPAGINATEDSERVICES:
+            return {
+                ...state,
+                currentServicesPage: action.payload.services,
+                totalPages: action.payload.totalCount // Almacena el número total de páginas en el estado.
             }
-
         default: return { ...state };
             };
 };
