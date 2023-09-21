794486e2082513643c3e35f94e45f1455e744503
LocoGOD <darioperez201864@gmail.com>
2023-09-21 18:52:59 -0300

    ?

--- src/components/Clasificados/Clasificados.jsx
+++ src/components/Clasificados/Clasificados.jsx
@@ -4,16 +4,20 @@ import style from './Clasificados.module.css';
 import { useEffect, useState } from 'react';
 import { getClasificados, postClasificados } from '../../redux/actions';
 import ModalClasificado from './ModalClasificado/ModalClasificado';
+import ModalPublicacion from './ModalPublicacion/ModalPublicacion';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const Clasificados = () => {
     const [show, setShow] = useState(false);
+    const [show2, setShow2] = useState(false);
+    const [publicacion, setPublicacion] = useState({});
     const handleClose = () => {
         setShow(false);
+        setShow2(false);
     };
     const dispatch = useDispatch();
     const { user, isAuthenticated } = useAuth0();
-    const handleSubmit = (value, event) => {        
+    const handleSubmit = (value, event) => {
         event.preventDefault();
         console.log(value);
         dispatch(postClasificados(value));
@@ -30,17 +34,26 @@ const Clasificados = () => {
         <div className={style.page}>
             <div className={style.container}>
                 {clasi.map((clasificado) => (
-                    <CardsClasificados
-                        picture={clasificado.image}
-                        tipo={clasificado.type}
-                        titulo={clasificado.title}
-                        descripcion={clasificado.description}
-                        contacto={clasificado.contact}
-                        precio={clasificado.price}
-                    />
+                    <button
+                        onClick={() => {
+                            console.log('click');
+                            setPublicacion(clasificado);
+                            setShow2(true);
+                        }}
+                        className={style.boton}
+                    >
+                        <CardsClasificados
+                            picture={clasificado.image}
+                            tipo={clasificado.type}
+                            titulo={clasificado.title}
+                            descripcion={clasificado.description}
+                            contacto={clasificado.contact}
+                            precio={clasificado.price}
+                        />
+                    </button>
                 ))}
             </div>
-            {true && (
+            {isAuthenticated && (
                 <button
                     className={style.button}
                     onClick={() => {
@@ -54,8 +67,12 @@ const Clasificados = () => {
                 show={show}
                 handleSubmit={handleSubmit}
                 handleClose={handleClose}
-                //email={user.email}
-                email="cristianbarth.1@gmail.com"
+                email={isAuthenticated ? user.email : ''}
+            />
+            <ModalPublicacion
+                show={show2}
+                handleClose={handleClose}
+                publicacion={publicacion}
             />
         </div>
     );
