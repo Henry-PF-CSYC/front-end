363c6b9a5a1957b4e6b377666ecf6f41a6bbae53
ptsuarez91@hotmail.com <ptsuarez91@hotmail.com>
2023-09-25 16:05:13 -0300

    estilos de servicios

--- src/components/Servicios/Agua/Agua.jsx
+++ src/components/Servicios/Agua/Agua.jsx
@@ -1,50 +1,55 @@
-import "./Agua.css" 
+import "./Agua.css"
 import CardsServicios from "../CardsServicios/CardsServicios"
 import {useSelector} from "react-redux"
 
 
 const Agua = () =>{
+const services=useSelector(state=>state.backUpServices)
+const aguaService=services.filter(service=>service.type==="agua")
 
-    
-    const services=useSelector(state=>state.backUpServices)
-    const aguaService=services.filter(service=>service.type==="agua")
+return(
+<section className="container-fluid aguaContainer">
+    <section id="aguaInfo">
+        <div className="titleService">
+        <h1>AGUA</h1>
+        </div>
+        <div className="containerDescription">
+            <div className="textoIzquierda">
+                <p >Nuestra suscripción de agua garantiza que tu hogar esté abastecido con agua potable de la más alta
+                    calidad. Nos comprometemos a proporcionar agua pura y segura para tu consumo diario y uso doméstico.
+                    Cada gota que fluye de nuestros grifos es sometida a rigurosos controles de calidad para garantizar
+                    que
+                    cumple con los estándares más estrictos.</p>
+            </div>
+            <div className="textoDerecha">
+                <p >Nuestra red de suministro de agua está diseñada para ser confiable y eficiente. No tendrás que
+                    preocuparte por interrupciones en el suministro. Además, ofrecemos un servicio de atención al
+                    cliente
+                    dedicado para resolver cualquier consulta o problema que puedas tener relacionado con el agua.</p>
+                <p >Tu suscripción de agua está activa y lista para proporcionarte el recurso vital que necesitas en tu
+                    hogar. Sabemos que la calidad del agua es esencial para tu salud y bienestar, y nos enorgullece ser
+                    tu
+                    proveedor de agua de confianza.</p>
+            </div>
+
+        </div>
+    </section>
 
 
-    return(
-        <section className="container-fluid aguaContainer">
-
-            <section id="aguaInfo">
-                <h1>Agua de calidad superior</h1>
-                
-                <p>Nuestra suscripción de agua garantiza que tu hogar esté abastecido con agua potable de la más alta calidad. Nos comprometemos a proporcionar agua pura y segura para tu consumo diario y uso doméstico. Cada gota que fluye de nuestros grifos es sometida a rigurosos controles de calidad para garantizar que cumple con los estándares más estrictos.</p>   
-                 
-                <p>Nuestra red de suministro de agua está diseñada para ser confiable y eficiente. No tendrás que preocuparte por interrupciones en el suministro. Además, ofrecemos un servicio de atención al cliente dedicado para resolver cualquier consulta o problema que puedas tener relacionado con el agua.</p> 
-                    
-                <p>Tu suscripción de agua está activa y lista para proporcionarte el recurso vital que necesitas en tu hogar. Sabemos que la calidad del agua es esencial para tu salud y bienestar, y nos enorgullece ser tu proveedor de agua de confianza.</p>
-            </section>
+    <hr />
 
 
-            <hr/>
+    <section className="row aguaCards">
+        {aguaService.map((service, index) => (
+        <div key={index} className="col-4">
+            <CardsServicios imagen={service.image} titulo={service.name} nombreBoton="Lo quiero!"
+                descripcion={service.description} precio={service.price} estado={service.status} type={service.type}
+                id={service.id} />
+        </div>
+        ))}
+    </section>
 
-
-            <section className="row aguaCards">
-                {aguaService.map((service, index) => (
-                    <div key={index} className="col-4">
-                        <CardsServicios
-                            imagen={service.image}
-                            titulo={service.name}
-                            nombreBoton="Lo quiero!"
-                            descripcion={service.description}
-                            precio={service.price}
-                            estado={service.status}
-                            type={service.type}
-                            id={service.id}
-                        />
-                    </div>
-                ))}
-            </section>
-            
-        </section>)
+</section>)
 }
 
 export default Agua;
