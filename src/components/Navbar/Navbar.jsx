47f7a910e89c8ef8cb126b4eba5946c56ecc5494
LocoGOD <darioperez201864@gmail.com>
2023-09-25 12:46:30 -0300

    Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import './Navbar.css';
 import { Link } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 import { emptyUser, getOfferByEmail, getUser } from '../../redux/actions';
+import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { IconCart } from '../Cart/IconCart';
 
